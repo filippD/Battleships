6e5ffd3ea23e26c9b97dda883f0c0c89d8186a2d
YellowPipe <philippdmitrienko@gmail.com>
2019-07-10 17:42:02 +0300

    working on randomized placement

--- src/gameboard.js
+++ src/gameboard.js
@@ -23,11 +23,94 @@ class Gameboard {
 		}
 	}
 
+	fillBoard() {
+		 // && this.ships[3] !== 2 && this.ships[2] !== 3 && this.ships[1] !== 4
+		 this.ships = {
+			'4': 0,
+			'3': 0,
+			'2': 0,
+			'1': 0
+		}
+		this.board = [
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null],
+			[null, null, null, null, null, null, null, null, null, null]
+		]
+		while(this.ships[4] !== 1) {
+			this.addShip(4, Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) )
+		}
+
+		while(this.ships[3] !== 2) {
+			this.addShip(3, Math.floor(Math.random()*10), Math.floor(Math.random()*10) )
+		}
+
+		while(this.ships[2] !== 3) {
+			this.addShip(2, Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) )
+		}
+
+		while(this.ships[1] !== 4) {
+			this.addShip(1, Math.floor(Math.random() * 10), Math.floor(Math.random() * 10) )
+		}
+	}
+
+	validatePosition(length, x, y) {
+		// let ok = true
+		// for (let i = y; y<=length; i++) {
+		// 	if (this.board[x][i] !== null) {
+		// 		ok = false 
+		// 	}
+			
+		// 	if (this.board[x-1][i] !== null || this.board[x+1][i] !== null) {
+		// 		ok = false 
+		// 	}
+		// }
+		// if (this.board[x][y-1] !== null || this.board[x][y+length] !== null) {
+		// 	ok = false 
+		// }
+		// return ok
+		let isOk = true;
+		if (y+length > 10) {
+			isOk = false
+		}
+		let minus = y-1 >= 0 ? y-1 : y
+		let plus = y+length+2 <= 9 ? y+length+2 : y+length+1
+		let myField = this.board[x].slice(minus, plus)
+		if(myField.every((val) => val !== null)) {
+			isOk = false 
+		}
+		if (x-1 >= 0) {
+			let myField2 = this.board[x-1].slice(minus, plus)
+			if(myField2.every((val) => val !== null)) {
+				isOk = false 
+			}
+		}
+
+		if (x+1 <= 9) {
+			let myField3 = this.board[x+1].slice(minus, plus)
+			if(myField3.every((val) => val !== null)) {
+				isOk = false 
+			}
+		}
+		
+		return isOk
+	}
+
 	addShip(length, x, y) {
-		this.ships[`${length}`] += 1;
-		const ship = new Ship(length);
-		for(let i = y; i< ship.length+y; i++) {
-			this.board[x][i] = ship
+		
+		if(this.validatePosition(length, x, y)) {
+			console.log(x,y)
+			this.ships[length]++;
+			const ship = new Ship(length);
+			for(let i = y; i<length+y; i++) {
+				this.board[x][i] = ship
+			}
 		}	
 	}
 
@@ -36,7 +119,7 @@ class Gameboard {
 			const ship = this.board[x][y];
 			ship.damage();
 			if (ship.isSunk()) {
-				this.ships[`${ship.length}`]-=1
+				this.ships[ship.length]--
 			}
 		}
 		this.board[x][y] = 'hit';
--- src/index.js
+++ src/index.js
@@ -12,14 +12,24 @@ const player1 = new Player('player');
 // 	})
 // }
 
+// const fillBoard = () => {
+
+// }
+
+
+
 const displayBoard = (player, board_id) => {
 	let myBoard = document.getElementById(board_id)
+	myBoard.innerHTML = null
 	for(let i=0; i<10; i++) {
 		let div = document.createElement('div');
 		for(let j = 0; j<10; j++) {
 			let innerDiv = document.createElement('div')
 			innerDiv.classList.add('sqr');
-			innerDiv.innerHTML=`${i}${j}`;
+			if (player.gameboard.board[i][j] !== null) {
+				innerDiv.classList.add('ship')
+			}
+			innerDiv.innerHTML=`x${i}y${j}`;
 			innerDiv.id=`${i}${j}`
 			// innerDiv.addEventListener('click', (e) => {
 			// 	player.gameboard.receiveAttack(Number(e.target.id[0]), Number(e.target.id[1]))
@@ -31,10 +41,12 @@ const displayBoard = (player, board_id) => {
 	}
 }
 
-// const placeShip(coord) = {
-	
-// }
+const randomize = (player) => {
+	player.gameboard.fillBoard()
+	displayBoard(player, 'board1')
+}
 
-
-
-displayBoard(player1, 'board1')
+const randButton = document.getElementById('Randomize')
+const dispButton = document.getElementById('Display')
+randButton.addEventListener('click', ()=>{randomize(player1)})
+// dispButton.addEventListener('click', ()=>{displayBoard(player1, 'board1')})
